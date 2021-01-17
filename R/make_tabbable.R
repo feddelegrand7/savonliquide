b5a86a347d8a21be6a916e1c2ce583a20402dab3
feddelegrand7 <ihaddaden.fodeil@gmail.com>
2021-01-17 10:41:41 +0100

    make_tabable adding example Rd

--- R/make_tabbable.R
+++ R/make_tabbable.R
@@ -3,7 +3,7 @@
 
 #' Make HTML elements tabable
 #'
-#' @param elements the HTML elements to be tabable (if not by default)
+#' @param element the HTML element to be tabable (if not by default)
 #' @param tab_index
 #' takes either 0, a negative or a positive value according to the required state of the element.
 #' 0 will make the element tabable with its relative order defined by the platform convention.
@@ -13,9 +13,29 @@
 #' @export
 #'
 #' @examples
+#'
+#' if (interactive()) {
+#'
+#'
+#'ui <- fluidPage(
+#'
+#'textInput(inputId = "inp1", label = "input"),
+#'
+#'    div(h1("Not tabable")) %>%
+#'        make_tabable(tab_index = -1),
+#'    div(h2("Tabable ! with priority")) %>%
+#'        make_tabable(tab_index = 1),
+#'    div(h2("Simply Tabable")) %>%
+#'        make_tabable(tab_index = 0))
+#'server <- function(input, output, session) {}
+#'
+#'shinyApp(ui = ui, server = server)
+#'
+#' }
 
 
-make_tabable <- function(elements, tab_index = 0) {
+
+make_tabable <- function(element, tab_index = 0) {
 
   tab_index <- as.character(tab_index)
 
